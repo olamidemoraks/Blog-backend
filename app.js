bd3a6bbeb1594b7743136bcfcdf8a7b52ce4b0a7
olamidemoraks <olamidemoraks@gmail.com>
2023-10-09 19:28:21 +0100

    adds url to cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ cloudinary.config({
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://blog-mymind.vercel.app/"],
     credentials: true,
   })
 );
